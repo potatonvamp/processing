e7cd04927c98efeb42543f1c6630c02eb7e677d0
potatonvamp <2026cambo.oly@fcboe.org>
2025-02-04 13:43:54 -0500

    Create ball.java

---
+++ ball.java
@@ -0,0 +1,14 @@
+class ball {
+  float x, y, radius, vx, vy, ax, ay;
+  int fill_color;
+  
+  public Ball(float x, float y, float radius, float vx, float vy, float ax, float ay, int fill_color);
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.vx = vx;
+  this.vy = vy;
+  this.fill_color = fill_color;
+  this.ax = ax;
+  this.ay = ay;
+}
